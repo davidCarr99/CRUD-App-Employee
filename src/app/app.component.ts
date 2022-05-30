1b72031f1edc38127b5e671f031e28ab72497f5f
LeoCarr <davidcarr1215@gmail.com>
2022-05-29 20:42:47 -0500

    agrega el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './modules/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'APP-Employees';
+  /*Arreglo para los empleados y mostrarlo en la interfaz*/
+  employeeArray: Employee[]=[
+    
+  ];
+
+  /*Formulario empleado*/
+  selectedEmployee: any =  {
+    name: '',
+    lastName: '',
+    phone: 0,
+    address: '',
+  };
+
+  /*Evento Añadir Empleado*/
+  addOrEdit(){
+    this.selectedEmployee.id = this.employeeArray.length +1;
+    this.employeeArray.push(this.selectedEmployee);
+    
+    /*Limpiar el Formulario*/
+    this.selectedEmployee =  {
+      name: '',
+      lastName: '',
+      phone: 0,
+      address: '',
+    };
+  }
+  /*Edtar el Empleado*/
+  edit(employee : Employee){
+    this.selectedEmployee = employee;
+  }
+
+  /*Eliminar Empleado*/
+  delete(){
+    if(confirm('Are you sure delete this employee?')){
+      this.employeeArray = this.employeeArray.filter(elem => elem != this.selectedEmployee);
+    }
+    this.selectedEmployee = {
+      name: '',
+      lastName: '',
+      phone: 0,
+      address: '',
+    }
+  }
 }
